6b2b4ac0f3cafdade5b1d82701865b7b0f03cc03
adpop <adpop@debian-11.8.0-beaglebone.myguest.virtualbox.org>
2024-03-29 19:27:29 -0700

    init commit. PRU not fully set up yet

---
+++ app/src/main.c
@@ -0,0 +1,11 @@
+// Main program to build the application
+// Has main(); does initialization and cleanup and perhaps some basic logic.
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main()
+{
+    printf("Hello world!\n");
+} 
